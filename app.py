e303bebc4d692db84dfd1c23cd0078682b7ae743
Saleh Eddine Touil <91285170+saleh-touil@users.noreply.github.com>
2022-05-14 00:44:46 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st  # pip3 install streamlit
+import lapin
+import os
+import time
+
+st.set_page_config(
+    page_title='Cleaner',layout="centered",page_icon=":rabbit:",
+    menu_items={'Get Help': None,'Report a bug': None,'About': None},initial_sidebar_state="auto")
+title = st.title('Combolist Cleaner :rabbit:')
+st.subheader('À vaillant coeur rien d’impossible. -Jacques Cœur')
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+#Upload fichier
+todo = []
+uploaded_file = st.file_uploader('Choisir un fichier txt ', type='txt')
+def write_to_file(text):
+      fb=open("base.txt","w",encoding="utf-8")
+      for i in range(len(text)):
+          fb.write(f"{todo[i]}\n")
+      fb.close()
+if uploaded_file:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode('utf-8')
+    todo = data.rsplit('\r\n')
+    write_to_file(todo)
+    with st.spinner('Wait for it...'):
+       tic = time.perf_counter()
+       lapin.main()
+       toc = time.perf_counter()
+    st.success(f'Done in {toc-tic:0.1f} Seconds !')
+    st.download_button(
+     label="Download file",
+     data=lapin.return_data(lapin.main.name),
+     file_name=lapin.main.name,
+     mime='text/plain')
---
+++ lapin.py
@@ -0,0 +1,57 @@
+from distutils.log import error
+import time
+from datetime import datetime
+import os
+def index(str,find):
+    index = str.find(find)
+    return index
+def copy_passwd(str,index):
+    passwd=str[index+1:]
+    return passwd
+
+def check_passwd(pas):
+    lenn=False
+    num,maj,min,=0,0,0
+    if len(pas)>=8:
+        lenn=True
+    for i in pas:
+        if i.isnumeric():
+            num = num+1
+        if i.isupper():
+            maj=maj+1
+        if i.islower():
+            min=min+1
+    return True if (maj!=0 and min!=0 and num!=0 and lenn) else False
+def return_result():
+    now = datetime.now()
+    current_time = now.strftime("%H-%M")
+    extension='.txt'
+    filename = current_time+extension
+    return filename
+def return_data(file):
+    f=open(file,'r+', encoding="utf8")
+    return f
+def main():
+    global name
+    todo=[]
+    f=open("base.txt","r+",encoding='utf-8')
+    main.name = return_result()
+    fr=open(main.name,'w+', encoding="utf8")
+    for line in f:
+        line = line.rstrip()
+        todo = line.split(':')
+        try:
+          if todo=="":
+              pass
+          mail = todo[0]
+          passwd = todo[1]
+        except:
+            error
+        if check_passwd(passwd):
+            fr.write(f"{mail}:{passwd}\n")
+
+if __name__ == "__main__":
+    tic = time.perf_counter()
+    main()
+    toc = time.perf_counter()
+    print(f"Done in {toc-tic:0.1f}")
